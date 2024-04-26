13ad5d453f2a353c47355b74a198f181b0a275b0
Mustafasohail7 <mustufasohail7@gmail.com>
2024-04-27 01:12:44 +0500

    bruh

--- cuda-tsp/taichi_test.py
+++ cuda-tsp/taichi_test.py
@@ -4,16 +4,24 @@ ti.init(arch=ti.cpu)
 
 lst = ti.field(dtype=ti.i32, shape=20)
 
+a = ti.field(ti.i32,shape=(5,5))
+b = ti.field(ti.i32,shape=(5,5))
+c = [1,2,3,4,5]
+
 @ti.kernel
 def test(a:ti.template()):
-    for i in range(10):
-        # check(i)
-        print(a)
+    for i in range(5):
+        check(i)
 
 @ti.func
-def check(x):
-    a = int(ti.random()*10)
-    print(a)
+def check(x: int):
+    for j in range(5):
+        a[x,j] = c[a[x,j]]
 
-for i in range(3):
-    test(i)
+for i in range(5):
+    for j in range(5):
+        a[i,j] = i+j
+
+print(a)
+test(a)
+print(a)
