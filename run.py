28c38e48502c2c79b2d59eb881a16f6b0381956e
Mustafasohail7 <mustufasohail7@gmail.com>
2024-04-27 09:00:13 +0500

    added file reading

---
+++ run.py
@@ -0,0 +1,22 @@
+# a function that reads qa194.tsp file and returns the data inform of number of cities and their coordinate array of x and y
+def read_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        data_x = []
+        data_y = []
+        nodes_start = False
+        for i in lines:
+            if i.startswith('NODE_COORD_SECTION'):
+                nodes_start = True
+                continue
+            elif i.startswith('EOF'):
+                nodes_start = False
+                break
+            elif i.startswith('DIMENSION'):
+                data = i.split()
+                dimension = int(data[2])
+            if nodes_start:
+                data = i.split()
+                data_x.append(float(data[1]))
+                data_y.append(float(data[2]))
+        return data_x, data_y, dimension
